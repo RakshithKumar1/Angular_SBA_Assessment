1bb2359f13a996c3bf0cf4a2c27872d1d60c48b0
Rakshit Kumar <mail.me.at.rakshith@gmail.com>
2023-07-04 15:14:06 +0530

    sba assessment

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { MyServiceService } from '../my-service.service';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent {
+
+  constructor(private service:MyServiceService){}
+
+  callServiceMethod(): void{
+    this.service.toDo("SBA assessment");
+  }
+}
